2019f40222f66d8df45c18ba898cd09298423380
Adrian Acosta <aacrealms@hotmail.com>
2021-04-15 02:52:32 +0000

    Implements additional endpoints

--- src/app.py
+++ src/app.py
@@ -1,4 +1,6 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+import json
+
 app = Flask(__name__)
 
 todos = [
@@ -8,8 +10,19 @@ todos = [
 
 @app.route('/todos', methods=['GET'])
 def hello_world():
-        json_text = jsonify(todos)        
-        return json_text
+    json_text = jsonify(todos)        
+    return json_text
+
+@app.route('/todos', methods=['POST'])
+def add_new_todo():
+    decoded_object = json.loads(request.data)
+    todos.append(decoded_object)
+    return jsonify(todos)
+
+@app.route('/todos/<int:position>', methods=['DELETE'])
+def delete_todo(position):
+    del todos[position]
+    return jsonify(todos)
 
 #Server definition
 if __name__ == '__main__':
